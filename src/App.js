b21e73e6a0ca4f867fad32f307b5a6ec4647728c
Jack Painter <littlesympathy@jacks-mbp.home>
2020-03-27 08:52:58 +0000

    Pull pokemon api and create PokemonsContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { PokemonsContainer } from './containers/PokemonsContainer'
 
 const App = () => {
+  const apiClient = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh'
+  })
+
   return (
-    <p>I am a pokemon</p>
-  )
+      <ApolloProvider apiClient={apiClient}>
+        <div>
+          <PokemonsContainer />
+        </div>
+      </ApolloProvider>
+    )
 }
 
 export default App;
---
+++ src/containers/PokemonsContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PokemonsContainer = () => {
+    return (
+        <div>I am a pokemon container</div>
+    )
+}
